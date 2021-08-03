d361438192892f19c9c76bd0ef6e615f0c0330fe
Maikon Ferreira <me@maikonferreira.tech>
2021-08-03 11:27:05 -0300

    adding app package

---
+++ app/src/main/java/com/maikkkko1/android_base_arq_app/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.maikkkko1.android_base_arq_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.maikkkko1.android_base_arq.arq.functions.getDaysList
+
+/**
+ * Created by Maikon Ferreira on 8/3/2021.
+ * me@maikonferreira.tech
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
